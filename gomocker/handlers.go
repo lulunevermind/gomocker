82da2159a5cca31f40f657f96c98cfbd85f5673a
jinx <lulunevermind@gmail.com>
2016-04-22 01:11:59 +0300

    references

--- gomocker/handlers.go
+++ gomocker/handlers.go
@@ -21,19 +21,17 @@ func handleMvd1(w http.ResponseWriter, r *http.Request) {
 
 		body := readBodyAsString(r)
 		if strings.Contains(body, "<deptcode>") {
-			fmt.Println("MATCH")
+			resp := mapping["mvd1.resp"]
+			fmt.Fprintf(w, resp)
 		}
-		resp := mapping["mvd1.resp"]
-		fmt.Fprintf(w, resp)
 	}
 }
 
 func logHandleRequest(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		r.ParseForm()       // parse arguments, you have to call this by yourself
-		fmt.Println(r.Form) // print form information in server side
-		fmt.Println(r.Method, r.URL.Path)
-		//		fmt.Println("scheme", r.URL.Scheme)
+		r.ParseForm()
+		fmt.Println(r.Form)
+		//		fmt.Println(readBodyAsString(r))
 		fmt.Println(r.Form["url_long"])
 		for k, v := range r.Form {
 			fmt.Println("key:", k)
